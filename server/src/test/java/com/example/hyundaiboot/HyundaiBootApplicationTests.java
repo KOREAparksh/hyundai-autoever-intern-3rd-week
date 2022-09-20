b8f65c614c5d32ca501d6a15f7982a9d1cbed061
KOREAparksh <tmdgks2222@naver.com>
2022-09-20 18:15:23 +0900

    🎉 Init: init spring boot project

---
+++ server/src/test/java/com/example/hyundaiboot/HyundaiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyundaiboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundaiBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
