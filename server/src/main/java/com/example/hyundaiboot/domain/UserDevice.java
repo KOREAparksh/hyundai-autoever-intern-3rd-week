95538920a63a0946bcbe5d164f989aabe3cfc8b9
KOREAparksh <tmdgks2222@naver.com>
2022-09-22 11:45:07 +0900

    ✨ Feat: Push 관련 클래스 구현

--- server/src/main/java/com/example/hyundaiboot/domain/UserDevice.java
+++ server/src/main/java/com/example/hyundaiboot/domain/UserDevice.java
@@ -1,16 +1,38 @@
 package com.example.hyundaiboot.domain;
 
 import lombok.Data;
+import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "USER_DEVICE_MGR")
 public class UserDevice {
-	@Column(name = "USER_ID")
-	private String id;
+//	@Id
+//	@Column(name = "USER_ID")
+//	private String user_id;
+
+	@Id
+	@ManyToOne
+	@ToString.Exclude
+	private User user;
+
+//	@Id
+//	@Column(name = "DEVICE_ID")
+//	private String device_id;
+
+	@Id
+	@ManyToOne
+	@ToString.Exclude
+	private DeviceMaster deviceMaster;
+
+	@Column(name = "MAX_RESENT_CNT")
+	private int max_sent_count;
+
+	@Column(name = "USER_DEVICE_DESC")
+	private String description;
+
+	@Column(name = "USE_YN")
+	private String useState;
 }
