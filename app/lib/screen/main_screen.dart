404f82e44bbe81f263467203030c6eb07fe1995b
KOREAparksh <tmdgks2222@naver.com>
2022-09-28 21:51:31 +0900

    ✨ Feat: Main 즐겨찾기 버튼 구현

--- app/lib/screen/main_screen.dart
+++ app/lib/screen/main_screen.dart
@@ -7,11 +7,50 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        hasStar: true,
-        title: "132213123213asdfasdfadsfasdfsdf",
+      appBar: CustomAppBar(),
+      body: Container(
+        child: Center(child: _FavoriteButton()),
       ),
-      body: Container(),
+    );
+  }
+}
+
+class _FavoriteButton extends StatelessWidget {
+  const _FavoriteButton({
+    Key? key,
+    this.title,
+  }) : super(key: key);
+
+  final _width = 136.0;
+  final _height = 136.0;
+  final _radius = 20.0;
+  final _padding = 10.0;
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: _width,
+      height: _height,
+      padding: EdgeInsets.all(_padding),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: (title == null) ? Color(0x1a000000) : Color(0x1a000000),
+        ),
+        borderRadius: BorderRadius.circular(_radius),
+      ),
+      child: Center(
+        child: Text(
+          title ?? "Blank",
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            color: (title != null) ? null : Color(0x66000000),
+            fontWeight: (title != null) ? FontWeight.bold : null,
+            fontSize: 16,
+          ),
+        ),
+      ),
     );
   }
 }
