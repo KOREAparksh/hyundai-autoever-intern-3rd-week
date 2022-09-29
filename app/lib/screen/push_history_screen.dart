e3ed647fde91e38937e4e95291fc98b99c36aef6
KOREAparksh <tmdgks2222@naver.com>
2022-09-29 20:05:44 +0900

    ✨ Feat: 이력 카드 light 버전 구현 및 grouped list view 구현

--- app/lib/screen/push_history_screen.dart
+++ app/lib/screen/push_history_screen.dart
@@ -1,6 +1,9 @@
+import 'package:app/const/Color.dart';
 import 'package:app/dto/push_history_dto.dart';
 import 'package:app/widget/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:intl/intl.dart';
 
 class PushHistoryScreen extends StatefulWidget {
   const PushHistoryScreen({Key? key}) : super(key: key);
@@ -29,8 +32,19 @@ class _PushHistoryScreenState extends State<PushHistoryScreen> {
       userName: "psh",
       deviceId: "1",
       deviceDescription: "note10",
-      pushTitle: "This is letter2",
-      pushContent: "2이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushTitle: "This is letter",
+      pushContent: "이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 05, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 05, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "note10",
+      pushTitle: "This is letter",
+      pushContent: "이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
       pushDateTime: DateTime.utc(2022, 08, 05, 12, 02, 43),
       sentDateTime: DateTime.utc(2022, 08, 05, 12, 04, 00),
       sentState: "Y",
@@ -42,8 +56,107 @@ class _PushHistoryScreenState extends State<PushHistoryScreen> {
       deviceDescription: "ipad",
       pushTitle: "3This is letter",
       pushContent: "3이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
-      pushDateTime: DateTime.utc(2022, 08, 05, 12, 02, 43),
-      sentDateTime: DateTime.utc(2022, 08, 05, 12, 04, 00),
+      pushDateTime: DateTime.utc(2022, 08, 06, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "2",
+      deviceDescription: "ipad",
+      pushTitle: "3This is letter",
+      pushContent: "3이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 06, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "2",
+      deviceDescription: "ipad",
+      pushTitle: "3This is letter",
+      pushContent: "3이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 06, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "2",
+      deviceDescription: "ipad",
+      pushTitle: "3This is letter",
+      pushContent: "3이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 06, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "2",
+      deviceDescription: "ipad",
+      pushTitle: "3This is letter",
+      pushContent: "3이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 06, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "2",
+      deviceDescription: "ipad",
+      pushTitle: "3This is letter",
+      pushContent: "3이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 06, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "note10",
+      pushTitle: "This is letter2",
+      pushContent: "2이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 07, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "note10",
+      pushTitle: "This is letter2",
+      pushContent: "2이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 07, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "note10",
+      pushTitle: "This is letter2",
+      pushContent: "2이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 07, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
+      sentState: "Y",
+    ),
+    PushHistoryDto(
+      userId: "1",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "note10",
+      pushTitle: "This is letter2",
+      pushContent: "2이 편지는 영국으로부터 시작되어 한국으로 온지 약 3년정도 된거 같기도 하고 아닌거 같기도 하고",
+      pushDateTime: DateTime.utc(2022, 08, 07, 12, 02, 43),
+      sentDateTime: DateTime.utc(2022, 08, 07, 12, 04, 00),
       sentState: "Y",
     ),
     PushHistoryDto(
@@ -53,8 +166,41 @@ class _PushHistoryScreenState extends State<PushHistoryScreen> {
       deviceDescription: "iphone",
       pushTitle: "iphone",
       pushContent: "steve jobs",
-      pushDateTime: DateTime.utc(2022, 08, 15, 12, 22, 43),
-      sentDateTime: DateTime.utc(2022, 08, 15, 14, 11, 00),
+      pushDateTime: DateTime.utc(2022, 08, 15),
+      sentDateTime: DateTime.utc(2022, 08, 15),
+      sentState: "N",
+    ),
+    PushHistoryDto(
+      userId: "2",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "iphone",
+      pushTitle: "iphone",
+      pushContent: "steve jobs",
+      pushDateTime: DateTime.utc(2022, 08, 15),
+      sentDateTime: DateTime.utc(2022, 08, 15),
+      sentState: "N",
+    ),
+    PushHistoryDto(
+      userId: "2",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "iphone",
+      pushTitle: "iphone",
+      pushContent: "steve jobs",
+      pushDateTime: DateTime.utc(2022, 08, 15),
+      sentDateTime: DateTime.utc(2022, 08, 15),
+      sentState: "N",
+    ),
+    PushHistoryDto(
+      userId: "2",
+      userName: "psh",
+      deviceId: "1",
+      deviceDescription: "iphone",
+      pushTitle: "iphone",
+      pushContent: "steve jobs",
+      pushDateTime: DateTime.utc(2022, 08, 15),
+      sentDateTime: DateTime.utc(2022, 08, 15),
       sentState: "N",
     ),
   ];
@@ -63,7 +209,114 @@ class _PushHistoryScreenState extends State<PushHistoryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: _title, hasStar: true),
-      body: Container(),
+      body: Container(
+        margin: EdgeInsets.only(left: 25, right: 25),
+        child: GroupedListView<PushHistoryDto, DateTime>(
+          elements: _list,
+          groupBy: (element) => element.pushDateTime,
+          groupSeparatorBuilder: (DateTime groupByValue) =>
+              Text(groupByValue.toString()),
+          itemBuilder: (context, PushHistoryDto element) =>
+              PushHistoryListTile(data: element),
+          useStickyGroupSeparators: true,
+          floatingHeader: true,
+          stickyHeaderBackgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
+
+class PushHistoryListTile extends StatelessWidget {
+  const PushHistoryListTile({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final PushHistoryDto data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 330,
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        border: Border.all(color: boxBorderDisable),
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.blue,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _form(child: _times()),
+          _form(child: _info()),
+          Container(height: 2, color: Colors.black),
+          _form(child: _pushTitle()),
+          _form(child: _pushContents()),
+        ],
+      ),
+    );
+  }
+
+  Widget _form({required Widget child}) {
+    return SizedBox(height: 25, child: child);
+  }
+
+  Widget _times() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Text("푸시", style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(width: 5),
+            Text(DateFormat("hh:mm:ss").format(data.pushDateTime)),
+          ],
+        ),
+        Row(
+          children: [
+            Text("수신", style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(width: 5),
+            Text(DateFormat("yy/MM/dd hh:mm:ss").format(data.sentDateTime)),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _info() {
+    return Row(
+      children: [
+        Expanded(
+            flex: 1,
+            child: Text(data.userName, overflow: TextOverflow.ellipsis)),
+        Expanded(flex: 2, child: Text(data.deviceDescription)),
+        Container(
+          width: 50,
+          height: 15,
+          color: Colors.green,
+          alignment: Alignment.center,
+          child: Text(
+            data.sentState,
+            style: TextStyle(color: Colors.white, fontSize: 12),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _pushTitle() {
+    return Container(
+        alignment: Alignment.centerLeft, child: Text(data.pushTitle));
+  }
+
+  Widget _pushContents() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        data.pushContent,
+        overflow: TextOverflow.ellipsis,
+      ),
     );
   }
 }
