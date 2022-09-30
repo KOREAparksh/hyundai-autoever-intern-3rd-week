38cf1903054266aaa91d58e1102c317d67c4cda6
KOREAparksh <tmdgks2222@naver.com>
2022-09-30 12:16:33 +0900

    ✨ Feat: UI 하드코딩 구현 및 Route 연결

--- app/lib/widget/custom_drawer.dart
+++ app/lib/widget/custom_drawer.dart
@@ -1,17 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class CustomDrawer extends StatelessWidget {
+class CustomDrawer extends StatefulWidget {
   const CustomDrawer({Key? key}) : super(key: key);
 
   @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  //asset
+  final _logo = "assets/autoever_logo.png";
+
+  //size
+  final _logoHeight = 30.0;
+  final _profileRadius = 40.0;
+  final _profileIconSize = 60.0;
+
+  //marginpadding
+  final _bodySideMargin = 15.0;
+
+  final _tileKey = GlobalKey<State>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Drawer(),
-      // child: Column(
-      //   children: [
-      //     Image.asset("assets/autoever_icon.png"),
-      //   ],
-      // ),
+    return Drawer(
+      child: Container(
+        margin: EdgeInsets.only(left: _bodySideMargin, right: _bodySideMargin),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue,
+              child: DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    children: [
+                      Image.asset(_logo, height: _logoHeight),
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: _profileRadius,
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                child: Icon(
+                                  Icons.person,
+                                  size: _profileIconSize,
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "123123",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text("345345",
+                                      overflow: TextOverflow.ellipsis),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ListTile(title: Text("홈")),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("모바일공통"),
+                      children: [
+                        ExpansionTile(
+                          title: Text("푸시알람"),
+                          children: [
+                            ListTile(
+                              title: Text("모바일기기등록"),
+                              onTap: () => Get.toNamed("/push/register"),
+                            ),
+                            ListTile(
+                              title: Text("푸시알람전송이력"),
+                              onTap: () => Get.toNamed("/push/history"),
+                            ),
+                          ],
+                        ),
+                        _Dummy(),
+                        _Dummy(),
+                      ],
+                    ),
+                    ExpansionTile(
+                      maintainState: true,
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("Production"),
+                      children: [
+                        _Dummy(),
+                        _Dummy(),
+                        _Dummy(),
+                      ],
+                    ),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("Order"),
+                      children: [
+                        _Dummy(),
+                        _Dummy(),
+                        _Dummy(),
+                      ],
+                    ),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("Progress"),
+                      children: [
+                        _Dummy(),
+                        _Dummy(),
+                        _Dummy(),
+                      ],
+                    ),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("상태"),
+                      children: [
+                        _Dummy(),
+                        _Dummy(),
+                        _Dummy(),
+                      ],
+                    ),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("Quality"),
+                    ),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("Equipment"),
+                    ),
+                    ExpansionTile(
+                      tilePadding: EdgeInsets.zero,
+                      title: Text("공통"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Dummy extends StatelessWidget {
+  const _Dummy({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text("Dummy"),
+      children: const [
+        ListTile(title: Text("Dummy")),
+        ListTile(title: Text("Dummy")),
+        ListTile(title: Text("Dummy")),
+      ],
     );
   }
 }
