358c8a353193b350d23b0493588085e152afcbc3
KOREAparksh <tmdgks2222@naver.com>
2022-10-02 17:11:31 +0900

    ✨ Feat: device_register screen, controller 분리

--- app/lib/controller/device_register_controller.dart
+++ app/lib/controller/device_register_controller.dart
@@ -1,5 +1,64 @@
 import 'package:app/controller/base_controller.dart';
+import 'package:app/dto/device_dto.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class DeviceRegisterController extends BaseController {}
+class DeviceRegisterController extends BaseController {
+  final List<DeviceDto> contentList = [];
+  final userIdController = TextEditingController();
+  final deviceIdController = TextEditingController();
+  final deviceController = TextEditingController();
+
+  void onTapAddButton() {}
+
+  void onTapSearchInit(void Function(void Function() fn) setState2) {
+    contentList.clear();
+    userIdController.clear();
+    deviceIdController.clear();
+    deviceController.clear();
+    list.forEach((element) => contentList.add(element));
+    setState2.call(() {});
+    Get.back();
+  }
+
+  void onTapSearchDialogPositive(void Function(void Function() fn) setState2) {
+    String _userId = userIdController.text;
+    String _deviceId = deviceIdController.text;
+    String _deviceKind = deviceController.text;
+    contentList.clear();
+    list.forEach((element) {
+      if (element.userId.contains(_userId) &&
+          element.deviceId.contains(_deviceId) &&
+          element.deviceKind.contains(_deviceKind)) {
+        contentList.add(element);
+      }
+    });
+    setState2.call(() {});
+    Get.back();
+  }
+
+  void onTapSearchDialogNegative() {
+    Get.back();
+  }
+
+  void onTapDelete(void Function(void Function() fn) setState2, int i) {
+    //Todo: Device Delete 통신 필요
+    contentList.removeAt(i);
+    setState2.call(() {});
+  }
+
+  final List<DeviceDto> list = const [
+    DeviceDto("WJKIM", "note10", "device1", "Android", 3, "Y"),
+    DeviceDto("WJKIM", "note11", "device2", "Android", 3, "N"),
+    DeviceDto("WJKIM", "note12", "device3", "Android", 3, "Y"),
+    DeviceDto("asdf", "note13", "device4", "Android", 3, "N"),
+    DeviceDto("asdf", "note14", "device5", "Android", 3, "Y"),
+    DeviceDto("asdfasdf", "note15", "device6", "Android", 3, "Y"),
+    DeviceDto("asdfasdf", "note16", "device7", "Android", 3, "N"),
+    DeviceDto("hyj", "note17", "device8", "Android", 3, "Y"),
+    DeviceDto("hyj", "note18", "device9", "Android", 3, "Y"),
+    DeviceDto("parkseunghan", "note19", "1device1", "Android", 3, "Y"),
+    DeviceDto("parkseunghan", "note20", "1device11", "Android", 3, "Y"),
+    DeviceDto("parkseunghan", "note21", "1device111", "Android", 3, "Y"),
+  ];
+}
