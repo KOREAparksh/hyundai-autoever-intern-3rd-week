550ec390e61efce40870e71ea20efa3a9a035dd9
KOREAparksh <tmdgks2222@naver.com>
2022-10-04 09:54:52 +0900

    ♻️ Refactor: CustomDialog error시 auto dialog 설정 기능 추가

--- app/lib/service/custom_dio.dart
+++ app/lib/service/custom_dio.dart
@@ -8,21 +8,29 @@ enum CustomDioError { notInternetConnected }
 
 class CustomDio {
   final Dio dio = Dio();
+  final bool autoDialog;
 
-  CustomDio() {
+  CustomDio({this.autoDialog = true}) {
     dio.options = BaseOptions(
       connectTimeout: 5000,
       receiveTimeout: 5000,
       baseUrl: dotenv.get("BASE_URL"),
     );
     dio.interceptors.clear();
-    dio.interceptors.add(CustomInterceptor());
+    dio.interceptors.add(CustomInterceptor(autoDialog: autoDialog));
   }
 }
 
 class CustomInterceptor extends InterceptorsWrapper {
+  final bool autoDialog;
+
+  CustomInterceptor({required this.autoDialog});
+
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (autoDialog == false) {
+      return handler.next(err);
+    }
     await showDialog(
       context: Get.context!,
       barrierDismissible: false,
