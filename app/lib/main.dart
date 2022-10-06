600c30350500dc18c21b88ed13bf586a4d0e809e
KOREAparksh <tmdgks2222@naver.com>
2022-10-07 00:35:35 +0900

    ✨ Feat: 모바일 화면회전 잠금 코드 추가

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,11 +6,17 @@ import 'package:app/screen/device_register_screen.dart';
 import 'package:app/screen/main_screen.dart';
 import 'package:app/screen/push_history_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
 void main() async {
   await dotenv.load(fileName: "env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   Get.put(MainController());
   runApp(const MyApp());
 }
